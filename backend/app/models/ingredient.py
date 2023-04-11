0163e75065bdda45ecda827b31b8b6981c915e6a
Marina <m_marine_f@mail.ru>
2023-04-11 09:45:29 +0300

    creat table, recipe, ingredient, user

---
+++ backend/app/models/ingredient.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, Float, ForeignKey, Text, String
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+from app.models.mixin import Timestamp
+from app.models.recipe_ingredient import RecipeIngredient
+
+
+class Ingredient(Timestamp, Base):
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, unique=True, index=True)
+
+    recipes = relationship("RecipeIngredient", back_populates="ingredient")
