7cd6a872c146d56db20d8ef9c79609ed43c558d2
Marina <m_marine_f@mail.ru>
2023-04-12 11:20:44 +0300

    done init recipe ingredient

--- backend/app/crud/recipe_ingredient.py
+++ backend/app/crud/recipe_ingredient.py
@@ -9,13 +9,27 @@ from app.models.recipe_ingredient import RecipeIngredient
 logger = logging.getLogger('recipebox')
 
 
-def create_recipe_ingredient(db: Session, recipe_ingredient: RecipeIngredient) -> List[RecipeIngredient]:
-    db_recipe_ingredient = RecipeIngredient(
+# def create_recipe_ingredient(db: Session, recipe_ingredient: ListRecipeIngredient) -> List[RecipeIngredient]:
+#     db_recipe_ingredient = RecipeIngredient(
+#         recipe_id=recipe_ingredient.recipe_id,
+#         ingredient_id=recipe_ingredient.ingredient_id,
+#         quantity=recipe_ingredient.quantity
+#     )
+#     # db.add(recipe_ingredient)
+#     # db.commit()
+#     # db.refresh(recipe_ingredient)
+#     logger.info(f"Recipe ingredient {recipe_ingredient} created")
+#     return db_recipe_ingredient
+
+
+def create_recipe_ingredient(db: Session, recipe_ingredient:  RecipeIngredient) -> RecipeIngredient:
+    recipe_ingredient = RecipeIngredient(
         recipe_id=recipe_ingredient.recipe_id,
         ingredient_id=recipe_ingredient.ingredient_id,
         quantity=recipe_ingredient.quantity
     )
-    logger.info(f"Recipe ingredient {recipe_ingredient} created")
-    return db_recipe_ingredient
-
+    db.add(recipe_ingredient)
+    db.commit()
+    db.refresh(recipe_ingredient)
+    return recipe_ingredient
 
--- backend/app/db/init_recipe_ingredient.py
+++ backend/app/db/init_recipe_ingredient.py
@@ -1,7 +1,10 @@
 import logging
 
 from app.crud.recipe_ingredient import create_recipe_ingredient
-from app.schemas.recipe_ingredient import RecipeIngredientModel
+#from app.schemas.recipe_ingredient import RecipeIngredientModel
+
+from app.schemas.recipe_ingredient import RecipeIngredientCreateModel
+
 # Set up logging configuration
 logging.basicConfig(level=logging.INFO)
 from sqlalchemy.orm import Session
@@ -22,14 +25,14 @@ def create_init_recipe_ingredient(db: Session):
     recipe_ingredient = settings.INIT_RECIPE_INGREDIENT
     if isinstance(recipe_ingredient, list):
         for r in recipe_ingredient:
-            db_recipe_ingredient = RecipeIngredientModel(
+            db_recipe_ingredient = RecipeIngredientCreateModel(
                 recipe_id=r['recipe_id'],
                 ingredient_id=r["ingredient_id"],
                 quantity=r['quantity']
             )
             create_recipe_ingredient(db, db_recipe_ingredient)
     else:
-        db_recipe_ingredient = RecipeIngredientModel(
+        db_recipe_ingredient = RecipeIngredientCreateModel(
             recipe_id=recipe_ingredient['recipe_id'],
             ingredient_id=recipe_ingredient["ingredient_id"],
             quantity=recipe_ingredient['quantity']
